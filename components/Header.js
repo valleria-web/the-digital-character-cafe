b6fe4fff84733be67635b778bf860fc2324af54e
¨vdsr¨ <valleria@gmail.com>
2023-04-20 20:00:06 +0200

    ¨all1¨

--- components/Header.js
+++ components/Header.js
@@ -13,9 +13,9 @@ export default function Header() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <Link href="https://www.odaee.org/">
+                        <Link href="/">
                             <a className='nav-link' target='_blank'>
-                                <li className="nav-item">cafe staff</li>
+                                <li className="nav-item">Cafe staff</li>
                             </a>
                         </Link>
                         <Link href="/staff">
