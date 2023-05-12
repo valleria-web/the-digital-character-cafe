b5e27d9f0c3ad0923510622b5513e399d2df9729
¨vdsr¨ <valleria@gmail.com>
2023-05-12 15:45:35 +0200

    ¨crossing-the-ordinal-portal¨

--- components/Header.js
+++ components/Header.js
@@ -13,15 +13,8 @@ export default function Header() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <Link href="/">
-                            <a className='nav-link' target='_blank'>
-                                <li className="nav-item">Cafe staff</li>
-                            </a>
-                        </Link>
-                        <Link href="/staff">
-                            <a className='nav-link'>
-                                <li className="nav-item">.</li>
-                            </a>
+                        <Link href="">
+                                <li className="nav-item text-white">Cafe staff</li>
                         </Link>
                     </ul>
                 </div>
