657e1af655c048f647cc3aca7bd5e72443f05169
¨vdsr¨ <valleria@gmail.com>
2023-05-13 15:52:24 +0200

    ¨banner¨

--- components/Banner.js
+++ components/Banner.js
@@ -4,16 +4,18 @@ export default function Banner() {
   return (
     <header className="py-2 bg-light border-bottom mb-4">
       <div className="container">
-        <div className="text-center my-5">
-          <Image
-            src="/images/logo.jpg"
-            alt="Logo"
-            width={100}
-            height={100}          
-          />
-          <h1 className="fw-bolder">The Digital Character Cafe</h1>
-          <p className="lead mb-0">Where Bitcoin NFTs come to life with unique personalities and stories. Its a quirky and imaginative space that explores the potential of Ordinals NFTs</p>
-        </div>
+        <Image
+          src="/images/our-team.jpg"
+          alt="Logo"
+          width={1500}
+          height={500}
+        />
+
+        <p className="lead mb-0">
+          Where Bitcoin NFTs come to life with unique personalities and stories.
+          Its a quirky and imaginative space that explores the potential of
+          Ordinals NFTs
+        </p>
       </div>
     </header>
   );
--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,30 @@
 import Link from 'next/link'
+
 export default function Header() {
-    return (<>
-
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <Link href="/">
-                    <a className='navbar-brand'>
-                        Bitcoin + NFTs
-                    </a>
-                </Link>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <Link href="">
-                                <li className="nav-item text-white">Cafe staff</li>
-                        </Link>
-                    </ul>
-                </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <Link href="/">
+          <a className='navbar-brand'>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <img src="./images/logo.jpg" alt="Digital Character Cafe Logo" width="100" height="100" style={{ borderRadius: '50%', marginRight: '10px' }} /> 
+              The Digital Character Cafe
             </div>
-        </nav>
-    </>
-    )
+          </a>
+        </Link>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <Link href="">
+              <li className="nav-item text-white">Cafe staff</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
